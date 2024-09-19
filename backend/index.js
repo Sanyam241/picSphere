082fee4cb8885d0b86bef61beb53023f82810dbf
sanyam jain <sanyamjain554@gmail.com>
2024-09-19 20:36:23 +0530

    first

---
+++ backend/index.js
@@ -0,0 +1,50 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDb from "./database/db.js";
+import cookieParser from "cookie-parser";
+import cloudinary from "cloudinary";
+import path from "path";
+
+
+dotenv.config();
+const app = express();
+
+cloudinary.v2.config({
+  cloud_name: process.env.Cloud_Name,
+  api_key: process.env.Cloud_Api,
+  api_secret: process.env.Cloud_Secret,
+});
+// app.use(cors({
+//   origin: 'http://localhost:5000', // Replace with your frontend URL
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   credentials: true, // If you're using cookies or other credentials
+// }));
+
+
+
+const port = process.env.PORT;
+
+//using middlewares
+app.use(express.json());
+app.use(cookieParser());
+
+// importing routes
+import userRoutes from "./routes/userRoutes.js";
+import pinRoutes from "./routes/pinRoutes.js";
+
+// using routes
+app.use("/api/user", userRoutes);
+app.use("/api/pin", pinRoutes);
+
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+  connectDb();
+});
